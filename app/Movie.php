191b28f92687966b163b18a8b6c289d9ebaaaa75
Federico <Bacino>
2020-04-22 08:47:55 -0300

    Creacion de modelos

---
+++ app/Movie.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Movie extends Model
+{
+    protected $guarded = [];
+
+    public function genre()
+    {
+        return $this->belongsTo(Genre::class);
+    }
+    public function actors()
+    {
+        return $this->belongsToMany(Actor::class)->withPivot('actor_id', 'movie_id');
+    }
+}
