197cf040beaed1b6290fae341a77eed1b2c89cf8
Federico <Bacino>
2020-04-28 16:42:26 -0300

    Fixes & Styles

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
 <section class="col-12 col-sm-12 col-md-9 pt-5">
     <div class="row">
         @foreach ($peliculas as $pelicula)
-        <div class="card venta col-12 col-sm-6 col-md-3 tarjetasSinborde pt-2" style>
+        <div class="card justify-content-center" style="width: 20rem;">
             <a href="/movies/detailMovie/{{ $pelicula->id }}" class="btn3">
                 <div class="card-body">
                     <h5 class="card-title">
